f85cebf01e16e8925bb9d6d6c5c64fe5492800ce
Marta Fonda <martafondapascual@gmail.com>
2016-02-02 20:44:03 +0100

    Adding some configuration for production performance with vulcanize and firebase

--- app/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-connect = require('gulp-connect');
-
-gulp.task('default', ['webserver']);
-gulp.task('default', function() {
-  connect.server();
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
 var gulp = require('gulp'),
-connect = require('gulp-connect');
+connect = require('gulp-connect'),
+vulcanize = require('gulp-vulcanize');
 
-gulp.task('default', ['webserver']);
-gulp.task('default', function() {
+gulp.task('default', ['copy','vulcanize','server']);
+
+gulp.task('server', function() {
   connect.server();
+});
+
+gulp.task('copy', function() {
+  return gulp.src([
+      'app/index.html',
+      'app/bower_components/webcomponentsjs/webcomponents-lite.js'
+    ],{
+      base: 'app'
+    })
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('vulcanize', function() {
+  return gulp.src('app/elements/elements.html')
+    .pipe(vulcanize({
+      stripComments: true,
+      inlineScripts: true,
+      inlineCss: true
+    }))
+    .pipe(gulp.dest('dist/elements'))
 });
